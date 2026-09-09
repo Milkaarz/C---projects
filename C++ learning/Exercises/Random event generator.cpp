cedf49f2af736591a045dd3a0c747e875420d727
Milkaarz <lpsgiovana@gmail.com>
2026-09-09 12:40:07 -0300

    Random event generator

--- C++ learning/Exercises/Random event generator.cpp
+++ C++ learning/Exercises/Random event generator.cpp
@@ -3,9 +3,31 @@
 #include <cstdlib>
 #include <ctime>
 
-int main(){
+int main() {
 
+    srand(time(0));
+    int randomNum = rand() % 5 + 1;
 
+    switch (randomNum) {
+        case 1: 
+            std::cout << "You win a bumper sticker!\n"; // Ponto e vírgula adicionado
+            break;
+        case 2: 
+            std::cout << "You win a t-shirt!\n";
+            break;
+        case 3: 
+            std::cout << "You win a pair of pants!\n";
+            break;
+        case 4: 
+            std::cout << "You win a sock!\n";
+            break;
+        case 5: 
+            std::cout << "You win a rose!\n";
+            break;
+        default:
+            std::cout << "Invalid prize.\n";
+            break;
+    }
 
-    
+    return 0;
 }
